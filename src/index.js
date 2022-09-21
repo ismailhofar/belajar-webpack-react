2b85eeb3b430e57d15000c15145d313a442bbe9c
Hofar Ismail <g9.hofar.ismail@gmail.com>
2022-09-21 18:15:55 +0700

    belajar

--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
-const express = require("express");
-const https = require("https");
-const app = express();
-app.get("/", function (req, res) {
-    res.send("hello world");
-});
-https.createServer(app);
-const PORT = 3000;
-const port = process.env.PORT || PORT;
-app.listen(port);
-console.log(`Running on ${PORT}`);
+import React from "react";
+import { ReactDOM } from "react";
+
+ReactDOM.render(<div>Halo dunia</div>, document.getElementById("app"));
