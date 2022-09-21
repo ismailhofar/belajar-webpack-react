be609cfc50617585a26a7f8f83fe56093578dd9b
Hofar Ismail <g9.hofar.ismail@gmail.com>
2022-09-21 19:01:43 +0700

    belajar

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<div>Halo dunia</div>, document.getElementById("app"));
+ReactDOM.render(<div>Hellow React's World</div>, document.getElementById("app"));
